0ba00f972093dda35e7f09683fce1c315acd9d26
Pieter Joost van de Sande <pj@born2code.net>
2015-02-18 22:15:30 +0100

    tidy: add levels

---
+++ level.go
@@ -0,0 +1,37 @@
+package logging
+
+const (
+	FATAL Level = iota
+	ERROR
+	WARN
+	NOTICE
+	INFO
+	DEBUG
+)
+
+var (
+	names = []string{
+		"FATAL",
+		"ERROR",
+		"WARN",
+		"NOTICE",
+		"INFO",
+		"DEBUG",
+	}
+	fixedNames = []string{
+		"FATAL ",
+		"ERROR ",
+		"WARN  ",
+		"NOTICE",
+		"INFO  ",
+		"DEBUG ",
+	}
+)
+
+func (this Level) String() string {
+	return names[this]
+}
+
+func (this Level) FixedString() string {
+	return fixedNames[this]
+}
