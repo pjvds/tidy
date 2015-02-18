5b118144fcf86dd3dc1123a7459318e239e3cb05
Pieter Joost van de Sande <pj@born2code.net>
2015-02-18 22:13:26 +0100

    tidy: add formatter benchmark

---
+++ text_test.go
@@ -0,0 +1,58 @@
+package slog
+
+import (
+	"testing"
+	"time"
+)
+
+var smallFields = Fields{
+	"foo": "bar",
+	"baz": 42,
+}
+
+type LengthRecorder struct {
+	Length int
+}
+
+func (this *LengthRecorder) Write(data []byte) (int, error) {
+	length := len(data)
+	this.Length = length
+
+	return length, nil
+}
+
+type DevNullWriter struct {
+}
+
+func (*DevNullWriter) Write(data []byte) (int, error) {
+	return len(data), nil
+}
+
+func BenchmarkColoredTextFormatter(b *testing.B) {
+	entry := Entry{
+		Module:    ModuleId("benchmark"),
+		Timestamp: time.Now(),
+		Level:     NOTICE,
+		Message:   "bazinga",
+		Fields: Fields{
+			"foo": "bar",
+			"baz": 42,
+		},
+	}
+
+	formatter := &ColoredTextFormatter{}
+	lengthRecorder := &LengthRecorder{}
+	devNull := &DevNullWriter{}
+
+	if err := formatter.FormatTo(lengthRecorder, entry); err != nil {
+		b.Fatalf("failed to write: %v", err.Error())
+	}
+	length := int64(lengthRecorder.Length)
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		b.SetBytes(length)
+		formatter.FormatTo(devNull, entry)
+	}
+}
