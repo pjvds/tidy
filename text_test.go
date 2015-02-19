490a16a3563da52b9dec0ae35950b79ebfd7931c
Pieter Joost van de Sande <pj@born2code.net>
2015-02-19 23:19:03 +0100

    tidy: add parallel test for text formatter

--- text_test.go
+++ text_test.go
@@ -28,6 +28,37 @@ func (*DevNullWriter) Write(data []byte) (int, error) {
 	return len(data), nil
 }
 
+func BenchmarkColoredTextFormatterParallel(b *testing.B) {
+	entry := Entry{
+		Module:    ModuleId("benchmark"),
+		Timestamp: time.Now(),
+		Level:     NOTICE,
+		Message:   "bazinga",
+		Fields: Fields{
+			"foo": "bar",
+			"baz": 42,
+		},
+	}
+
+	formatter := &ColoredTextFormatter{}
+	lengthRecorder := &LengthRecorder{}
+	devNull := &DevNullWriter{}
+
+	if err := formatter.FormatTo(lengthRecorder, entry); err != nil {
+		b.Fatalf("failed to write: %v", err.Error())
+	}
+	length := int64(lengthRecorder.Length)
+
+	b.ResetTimer()
+	b.SetBytes(length)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			formatter.FormatTo(devNull, entry)
+		}
+	})
+}
+
 func BenchmarkColoredTextFormatter(b *testing.B) {
 	entry := Entry{
 		Module:    ModuleId("benchmark"),
