b0ba49f13a1f7f072d70fd3b593a23961c6a807e
Pieter Joost van de Sande <pj@born2code.net>
2015-02-21 11:04:39 +0100

    tidy: add Fields unit tests

---
+++ fields_test.go
@@ -0,0 +1,43 @@
+package logging
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFieldsInitialLen(t *testing.T) {
+	var uninitialized Fields
+	assert.Equal(t, 0, uninitialized.Len())
+
+	assert.Equal(t, 1, Fields{
+		"foo": "bar",
+	}.Len())
+}
+
+func TestFieldsClone(t *testing.T) {
+	fields := Fields{
+		"foo": "bar",
+		"baz": 42,
+	}
+
+	cloned := fields.Clone(0)
+	assert.Equal(t, fields.Len(), cloned.Len())
+
+	for key, value := range fields {
+		assert.Equal(t, value, cloned[key])
+	}
+}
+
+func TestFieldsCloneDoesNotEffectLen(t *testing.T) {
+	fields := Fields{
+		"foo": "bar",
+		"baz": 42,
+	}
+
+	// specify a positive grow size to test
+	// that this has no influence on the reported length
+	cloned := fields.Clone(5)
+
+	assert.Equal(t, fields.Len(), cloned.Len())
+}
