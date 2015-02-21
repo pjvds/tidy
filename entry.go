49174691cc91f941be8892b96539fc66313f515a
Pieter Joost van de Sande <pj@born2code.net>
2015-02-21 15:20:28 +0100

    tidy: adds documentation to Entry

--- entry.go
+++ entry.go
@@ -2,11 +2,20 @@ package logging
 
 import "time"
 
+// Represents a single log entry.
 type Entry struct {
+	// The time this entry was created.
 	Timestamp time.Time
-	Module    ModuleId
-	Level     Level
 
+	// The module this entry belongs to.
+	Module ModuleId
+
+	// The level of this entry, eq: DEBUG, WARN, FATAL.
+	Level Level
+
+	// The final message.
 	Message string
-	Fields  Fields
+
+	// The fields for this entry.
+	Fields Fields
 }
