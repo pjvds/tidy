d04e8fb95334f00a1d6e6043a030a85bd2a12f35
Pieter Joost van de Sande <pj@born2code.net>
2015-02-21 11:04:26 +0100

    tidy: add documentation to Fields

--- fields.go
+++ fields.go
@@ -2,14 +2,19 @@ package logging
 
 type Fields map[string]interface{}
 
+// Len returns the number of fields in the instance.
 func (this Fields) Len() int {
 	return len(this)
 }
 
+// Any returns true when there is any field in this instance; otherwise false.
 func (this Fields) Any() bool {
 	return len(this) > 0
 }
 
+// Clone creates a new Fields instance that holds the same values
+// as this instance with an increased capacity this instance plus
+// the specified grow size.
 func (this Fields) Clone(grow int) Fields {
 	clone := make(Fields, this.Len()+grow)
 
@@ -20,6 +25,8 @@ func (this Fields) Clone(grow int) Fields {
 	return clone
 }
 
+// Join creates a new Fields instance with the content of this instance and
+// the specified one.
 func (this Fields) Join(fields Fields) Fields {
 	joined := this.Clone(fields.Len())
 
