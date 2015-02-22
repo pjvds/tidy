18638fd4ebd27c473aa55f1626118ac035ddf3bb
Pieter Joost van de Sande <pj@born2code.net>
2015-02-22 08:53:33 +0100

    tidy: add plain text formatter

---
+++ plain.go
@@ -0,0 +1,35 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+)
+
+type PlainTextFormatter struct{}
+
+func (PlainTextFormatter) FormatTo(writer io.Writer, entry Entry) error {
+	buffer := NewBuffer()
+	defer buffer.Free()
+
+	buffer.WriteString(entry.Timestamp.Format("15:04:05.000 "))
+	buffer.WriteString(entry.Level.FixedString())
+	buffer.Write(whitespace)
+	buffer.WriteString(entry.Module.String())
+	buffer.Write(whitespace)
+	buffer.WriteString(entry.Message)
+
+	if entry.Fields.Any() {
+		buffer.WriteString("\t")
+		for key, value := range entry.Fields {
+			buffer.Write(whitespace)
+			buffer.WriteString(key)
+			buffer.WriteString("=")
+			buffer.WriteString(fmt.Sprint(value))
+		}
+	}
+
+	buffer.Write(newline)
+
+	_, err := buffer.WriteTo(writer)
+	return err
+}
