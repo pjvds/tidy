c0237647d20119ee96815f00f25d0d31e4f34c0c
Pieter Joost van de Sande <pj@born2code.net>
2015-02-23 19:46:56 +0100

    tidy: add module from caller support

--- module.go
+++ module.go
@@ -1,11 +1,36 @@
 package logging
 
-import "reflect"
+import (
+	"errors"
+	"reflect"
+	"runtime"
+	"strings"
+)
 
 type ModuleId string
 
 func GetModule(value interface{}) ModuleId {
 	return ModuleId(reflect.TypeOf(value).PkgPath())
+}
+
+func GetModuleFromCaller(depth int) ModuleId {
+	pc, _, _, ok := runtime.Caller(1 + depth)
+
+	if !ok {
+		panic(errors.New("failed to get caller from runtime"))
+	}
+
+	function := runtime.FuncForPC(pc)
+
+	if function == nil {
+		panic(errors.New("failed to get function from program counter"))
+	}
+
+	name := function.Name()
+	lastSlashIndex := strings.LastIndex(name, "/")
+	lastDotIndex := strings.LastIndex(name, ".")
+
+	return ModuleId(name[lastSlashIndex+1 : lastDotIndex])
 }
 
 func (this ModuleId) String() string {
--- module_test.go
+++ module_test.go
@@ -6,10 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var moduleAtInit = GetModuleFromCaller(0)
+
 type MyStructure struct{}
 
 func TestGetModule(t *testing.T) {
 	module := GetModule(MyStructure{})
 
 	assert.Equal(t, module.String(), "github.com/pjvds/logging")
+}
+
+func TestGetModuleFromCaller(t *testing.T) {
+	module := GetModuleFromCaller(0)
+
+	assert.Equal(t, module.String(), "logging")
+}
+
+func TestGetModuleFromCallerAtInit(t *testing.T) {
+	assert.Equal(t, moduleAtInit.String(), "logging")
 }
