f46a2f8f16e2c25e1f6f5c8a8c60e5d74b66f32c
Pieter Joost van de Sande <pj@born2code.net>
2015-02-28 09:44:21 +0100

    logentries: move config to own file

--- logentries/backend.go
+++ logentries/backend.go
@@ -8,54 +8,6 @@ import (
 	"github.com/pjvds/tidy"
 )
 
-const (
-	network = "udp"
-	address = "data.logentries.com"
-	port    = 20000
-)
-
-type Config struct {
-	address string
-	network string
-	token   string
-}
-
-func Configure(token string) Config {
-	return Config{
-		address: "data.logentries.com:10000",
-		network: "udp",
-		token:   token,
-	}
-}
-
-func (this Config) Token(value string) Config {
-	this.token = value
-	return this
-}
-
-func (this Config) UDP() Config {
-	this.network = "udp"
-	return this
-}
-
-func (this Config) TCP() Config {
-	this.network = "tcp"
-	return this
-}
-
-func (this Config) Build() tidy.Backend {
-	b := &backend{
-		entries:   make(chan tidy.Entry, 10000),
-		network:   this.network,
-		address:   this.address,
-		formatter: tidy.PlainTextFormatter{},
-		token:     []byte(this.token),
-	}
-	go b.do()
-
-	return b
-}
-
 type backend struct {
 	entries   chan tidy.Entry
 	network   string
---
+++ logentries/config.go
@@ -0,0 +1,45 @@
+package logentries
+
+import "github.com/pjvds/tidy"
+
+type Config struct {
+	address string
+	network string
+	token   string
+}
+
+func Configure(token string) Config {
+	return Config{
+		address: "data.logentries.com:10000",
+		network: "udp",
+		token:   token,
+	}
+}
+
+func (this Config) Token(value string) Config {
+	this.token = value
+	return this
+}
+
+func (this Config) UDP() Config {
+	this.network = "udp"
+	return this
+}
+
+func (this Config) TCP() Config {
+	this.network = "tcp"
+	return this
+}
+
+func (this Config) Build() tidy.Backend {
+	b := &backend{
+		entries:   make(chan tidy.Entry, 10000),
+		network:   this.network,
+		address:   this.address,
+		formatter: tidy.PlainTextFormatter{},
+		token:     []byte(this.token),
+	}
+	go b.do()
+
+	return b
+}
