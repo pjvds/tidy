3942f83aea120b66bf3edab67170810d860ca044
Pieter Joost van de Sande <pj@born2code.net>
2015-03-04 20:59:51 +0100

    logentries: add buffer size configuration

--- logentries/config.go
+++ logentries/config.go
@@ -3,17 +3,24 @@ package logentries
 import "github.com/pjvds/tidy"
 
 type Config struct {
-	address string
-	network string
-	token   string
+	address    string
+	network    string
+	token      string
+	bufferSize int
 }
 
 func Configure(token string) Config {
 	return Config{
-		address: "data.logentries.com:10000",
-		network: "udp",
-		token:   token,
+		address:    "data.logentries.com:10000",
+		network:    "udp",
+		token:      token,
+		bufferSize: 256,
 	}
+}
+
+func (this Config) BufferSize(size int) Config {
+	this.bufferSize = size
+	return this
 }
 
 func (this Config) Token(value string) Config {
@@ -33,7 +40,7 @@ func (this Config) TCP() Config {
 
 func (this Config) Build() tidy.Backend {
 	b := &backend{
-		entries:   make(chan tidy.Entry, 10000),
+		entries:   make(chan tidy.Entry, this.bufferSize),
 		network:   this.network,
 		address:   this.address,
 		formatter: tidy.PlainTextFormatter{},
