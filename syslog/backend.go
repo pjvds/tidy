d5626244fc056780686f70202aa5d3093f69676e
Pieter Joost van de Sande <pj@born2code.net>
2015-03-14 09:32:13 +0100

    syslog: adds syslog backend and config

--- syslog/backend.go
+++ syslog/backend.go
@@ -1,16 +1,59 @@
 package syslog
 
-//import(sys "log/syslog")
+import (
+	"bytes"
+	. "log/syslog"
+
+	"github.com/pjvds/tidy"
+)
+
 //Only one call to Dial is necessary. On write failures, the syslog client will attempt to reconnect to the server and write again.
-
 type backend struct {
-	// The network (udp, tcp), or empty for local syslog socket. If the network is set
-	// you also need to set the address.
-	Network string
-	// The remote address, or empty for local syslog socket. If the network is set
-	// you also need to set the address.
-	Address string
+	entries   chan tidy.Entry
+	writer    *Writer
+	formatter tidy.PlainTextFormatter
+}
 
-	// The TAG field value for the syslog entries. If empty the process name is used.
-	Tag string
+func (this *backend) do() {
+	buffer := new(bytes.Buffer)
+
+	for entry := range this.entries {
+		buffer.Reset()
+
+		if err := this.formatter.FormatTo(buffer, entry); err != nil {
+			// todo: add internal debug logging
+			// not much we can do about it
+			continue
+		}
+
+		switch entry.Level {
+		case tidy.FATAL:
+			this.writer.Crit(buffer.String())
+		case tidy.ERROR:
+			this.writer.Err(buffer.String())
+		case tidy.WARN:
+			this.writer.Warning(buffer.String())
+		case tidy.NOTICE:
+			this.writer.Notice(buffer.String())
+		case tidy.INFO:
+			this.writer.Info(buffer.String())
+		case tidy.DEBUG:
+			this.writer.Debug(buffer.String())
+		default:
+			// todo: add internal debug logging
+			// not much we can do about it
+		}
+	}
+}
+
+func (this *backend) IsEnabledFor(level tidy.Level, module tidy.Module) bool {
+	return true
+}
+
+func (this *backend) Log(entry tidy.Entry) {
+	this.entries <- entry
+}
+
+func (this *backend) Flush() error {
+	return nil
 }
---
+++ syslog/config.go
@@ -0,0 +1,69 @@
+package syslog
+
+import (
+	. "log/syslog"
+
+	"github.com/pjvds/tidy"
+)
+
+type Config struct {
+	// The network (udp, tcp), or empty for local syslog socket. If the network is set
+	// you also need to set the address.
+	network string
+	// The remote address, or empty for local syslog socket. If the network is set
+	// you also need to set the address.
+	address string
+
+	// The TAG field value for the syslog entries. If empty the process name is used.
+	tag string
+
+	// The buffer size of the channel.
+	bufferSize int
+}
+
+// Configure a new syslog backend. Call `Config.Build` to build
+// the backend instance.
+func Configure() Config {
+	return Config{}
+}
+
+// Overrides the default local syslog address. Usually only used when logging to
+// a remote syslog server.
+func (this Config) Address(network string, address string) Config {
+	this.network = network
+	this.address = address
+	return this
+}
+
+// Overrides the default tag that is set to the process name (os.Args[0]).
+func (this Config) Tag(tag string) Config {
+	this.tag = tag
+	return this
+}
+
+// Sets the buffer size of the number of entries that can
+// be held in memory. When the buffer is full, `Backend.Log` becomes blocking.
+func (this Config) BufferSize(size int) Config {
+	// TODO: allow to set block behavior
+	this.bufferSize = size
+	return this
+}
+
+// Build the backend based on the config.
+func (this Config) Build() tidy.Backend {
+	writer, err := Dial(this.network, this.address, LOG_DEBUG, this.tag)
+
+	// Dial only errors when there are invalid parameters, not when it can connect.
+	if err != nil {
+		panic(err)
+	}
+
+	b := &backend{
+		entries:   make(chan tidy.Entry, this.bufferSize),
+		formatter: tidy.PlainTextFormatter{},
+		writer:    writer,
+	}
+	go b.do()
+
+	return b
+}
