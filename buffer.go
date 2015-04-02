be782e10c37337248c3c9be88c6b81bdd28d1f0b
Pieter Joost van de Sande <pj@born2code.net>
2015-04-02 21:28:59 +0200

    tidy: remove public buffer size const

--- buffer.go
+++ buffer.go
@@ -9,15 +9,11 @@ var (
 	buffers *sync.Pool
 )
 
-const (
-	INITIAL_BUFFER_SIZE = 1000
-)
-
 func init() {
 	buffers = &sync.Pool{
 		New: func() interface{} {
 			buffer := new(FreeableBuffer)
-			buffer.Grow(INITIAL_BUFFER_SIZE)
+			buffer.Grow(1000)
 
 			return buffer
 		},
