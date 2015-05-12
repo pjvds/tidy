253398e507e6f804d766a3181198cffff3592eb3
Pieter Joost van de Sande <pj@born2code.net>
2015-05-12 21:44:54 +0200

    test(appengine): add test for backend

---
+++ appengine/backend_test.go
@@ -0,0 +1,26 @@
+package appengine
+
+import (
+	"testing"
+
+	"github.com/pjvds/tidy"
+	"google.golang.org/appengine/internal"
+
+	"golang.org/x/net/context"
+)
+
+func TestBackendInvokesInternalLogger(t *testing.T) {
+	logger := tidy.Configure().LogFromLevel(tidy.DEBUG).To(Configure()).MustBuild()
+
+	invoked := false
+	ctx := internal.WithLogOverride(context.Background(), func(level int64, format string, args ...interface{}) {
+		invoked = true
+		return
+	})
+
+	logger.Context(ctx).Debug("foobar")
+
+	if !invoked {
+		t.Fatalf("internal logger never called")
+	}
+}
