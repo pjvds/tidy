486905d0869f89e43d123cda64bc388c8fbf51b2
Pieter Joost van de Sande <pj@born2code.net>
2015-05-12 22:01:30 +0200

    test(appengine): test argument passing

--- appengine/backend_test.go
+++ appengine/backend_test.go
@@ -6,15 +6,23 @@ import (
 	"github.com/pjvds/tidy"
 	"google.golang.org/appengine/internal"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
 
 func TestBackendInvokesInternalLogger(t *testing.T) {
 	logger := tidy.Configure().LogFromLevel(tidy.DEBUG).To(Configure()).MustBuild()
 
-	invoked := false
+	var invoked bool
+	var passedLevel int64
+	var passedFormat string
+	var passedArgs []interface{}
+
 	ctx := internal.WithLogOverride(context.Background(), func(level int64, format string, args ...interface{}) {
 		invoked = true
+		passedLevel = level
+		passedFormat = format
+		passedArgs = args
 		return
 	})
 
@@ -23,6 +31,9 @@ func TestBackendInvokesInternalLogger(t *testing.T) {
 	if !invoked {
 		t.Fatalf("internal logger never called")
 	}
+
+	assert.Equal(t, 0, passedLevel)
+	assert.Equal(t, "DEBUG (tidy.config): foobar", passedFormat)
 }
 
 func TestBackendDoesNotPanicOnNilContext(t *testing.T) {
