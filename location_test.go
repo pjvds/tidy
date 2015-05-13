914d7c9f4e675966e50f6b760673fdf1d8a58132
Pieter Joost van de Sande <pj@born2code.net>
2015-05-13 19:24:43 +0200

    test(location): add test for IsEmpty

--- location_test.go
+++ location_test.go
@@ -13,6 +13,14 @@ func TestGetLocation(t *testing.T) {
 	assert.Equal(t, location.String(), "location_test.go:11")
 }
 
+func TestLocationIsEmpty(t *testing.T) {
+	notEmpty := tidy.Location("location_test.go:11")
+	empty := tidy.Location("")
+
+	assert.False(t, notEmpty.IsEmpty())
+	assert.True(t, empty.IsEmpty())
+}
+
 func BenchmarkGetLocation(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		tidy.GetLocation(0)
