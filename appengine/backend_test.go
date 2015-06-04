185fbaff0111acca215d3b721c2244bf540e5672
Pieter Joost van de Sande <pj@born2code.net>
2015-06-04 11:16:21 +0200

    fix(appengine): include location in expectation

--- appengine/backend_test.go
+++ appengine/backend_test.go
@@ -33,7 +33,7 @@ func TestBackendInvokesInternalLogger(t *testing.T) {
 	}
 
 	assert.Equal(t, int64(0), passedLevel)
-	assert.Equal(t, "DEBUG (appengine): foobar", passedFormat)
+	assert.Equal(t, "DEBUG (appengine): foobar\t location=backend_test.go:29", passedFormat)
 }
 
 func TestBackendDoesNotPanicOnNilContext(t *testing.T) {
